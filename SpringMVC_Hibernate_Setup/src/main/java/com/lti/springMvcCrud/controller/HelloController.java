a3fa1fc11b544c9e38a701e7d6e312b6b05250e5
mukul <mukulbabel@gmail.com>
2019-11-22 12:28:08 +0530

    "commit"

--- SpringMVC_Hibernate_Setup/src/main/java/com/lti/springMvcCrud/controller/HelloController.java
+++ SpringMVC_Hibernate_Setup/src/main/java/com/lti/springMvcCrud/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.lti.springSecurityconfig;
+package com.lti.springMvcCrud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
